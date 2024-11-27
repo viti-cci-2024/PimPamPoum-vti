2fac9818f431ff4e5044375a2012ecdab5b4066e
TymeoGuedel <t.guedel@cci.formation.nc>
2024-11-27 15:29:02 +1100

    v2 footer

--- js/footer.js
+++ js/footer.js
@@ -5,23 +5,46 @@ class Footer extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <footer class="footer bg-dark text-light py-5">
+            <footer class="footer py-5">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4">
-                            <h5>Gîte Pim</h5>
-                            <p>Évasion au naturel : vivez l'authenticité de l'île de Pam.</p>
+                        <!-- Colonne 1 : Logo et description -->
+                        <div class="col-md-3 mb-4">
+                            <img src="./public/logo-gite-BLANC.png" alt="Logo Gîte Pim" width="200" class="mb-3">
+                             <h6 class="text-uppercase fw-bold text-white mb-3">Contact</h6>
+                            <p class="text-white"><i class="bi bi-geo-alt-fill me-2"></i> Îlot Pam, Poum</p>
+                            <p class="text-white"><i class="bi bi-telephone-fill me-2"></i> +687 12 34 56</p>
+                            <p class="text-white"><i class="bi bi-envelope-fill me-2"></i> contact@gitepim.nc</p>
+                            
                         </div>
-                        <div class="col-md-4">
-                            <h5>Navigation</h5>
+                        <!-- Colonne 2 : Navigation principale -->
+                        <div class="col-md-3 mb-4">
+                            <h6 class="text-uppercase fw-bold text-white mb-3">Navigation</h6>
                             <ul class="list-unstyled">
-                                <li><a href="./index.html" class="text-light">Accueil</a></li>
-                                <li><a href="./modules/chambre/chambre-index.html" class="text-light">Nos chambres</a></li>
+                                <li><a href="./index.html" class="text-white text-decoration-none">Accueil</a></li>
+                                <li><a href="./modules/chambre/chambre-index.html" class="text-white text-decoration-none">Nos chambres</a></li>
+                                <li><a href="./modules/repas/repas-index.html" class="text-white text-decoration-none">Notre restaurant</a></li>
+                                
+                                <li><a href="./modules/garderie/garderie-index.html" class="text-white text-decoration-none">La garderie</a></li></br></br>
+                            </ul>
+                       
+                      
+                            <h6 class="text-uppercase fw-bold text-white mb-3">Activité</h6>
+                            <ul class="list-unstyled">
+                                <li><a href="./modules/cheval/cheval-index.html" class="text-white text-decoration-none">Randonnée équestre</a></li>
+                                <li><a href="./modules/kayak/kayak-index.html" class="text-white text-decoration-none">Sortie en kayak</a></li>
+                                <li><a href="./modules/bagne/bagne-index.html" class="text-white text-decoration-none">Visite du bagne</a></li>
                             </ul>
                         </div>
-                        <div class="col-md-4">
-                            <h5>Suivez-nous</h5>
-                            <a href="#" class="text-light">Facebook</a>
+                        <!-- Colonne 3 : Contact et réseaux sociaux -->
+                        <div class="col-md-3 mb-4">
+                        <img src="./public/cartepim.png" alt="lieux Gîte Pim" width="300" class="mb-3">
+                           <h6 class="text-uppercase fw-bold text-white mt-4">Suivez-nous</h6>
+                            <div>
+                                <a href="#" class="text-white me-3"><img src="./public/facebook.png" alt="Facebook" width="24"></a>
+                                <a href="#" class="text-white me-3"><img src="./public/instagram.png" alt="Instagram" width="24"></a>
+                                <a href="#" class="text-white"><img src="./public/twitter.png" alt="Twitter" width="24"></a>
+                            </div>
                         </div>
                     </div>
                 </div>

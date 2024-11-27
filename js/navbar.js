76f4baba19e67ca551582018676fef9b6ada2627
viti cci 2024 <p.viotti@cci-formation.nc>
2024-11-27 13:40:25 +1100

    mise en place navbar sur main

--- js/navbar.js
+++ js/navbar.js
@@ -5,53 +5,39 @@ class NavBar extends HTMLElement {
 	}
 	connectedCallback() {
 		this.innerHTML = `
-			<header>
-				<nav class="navbar not-mobile">
-					<ul>
-						<li>
-							<a href="../../index.html">
-								<img src="../../public/logo.ico" alt="Accueil" title="Accueil">
-							</a>
-						</li>
-						<li>
-							<a href="../chambre/chambre-index.html">
-								Nos chambres
-							</a>
-						</li>
-						<li>
-							<a href="../repas/repas-index.html">
-								Notre restaurant
-							</a>
-						</li>
-						<li>
-							<a href="../cheval/cheval-index.html">
-								Randonnée équestre
-							</a>
-						</li>
-						<li>
-							<a href="../kayak/kayak-index.html">
-								Sortie en kayak
-							</a>
-						</li>
-						<li>
-							<a href="../bagne/bagne-index.html">
-								Visite du bagne
-							</a>
-						</li>
-						<li>
-							<a href="../garderie/garderie-index.html">
-								La garderie
-							</a>
-						</li>
-					</ul>
-				</nav>
+			<nav class="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.html">
+      <img src="./public/logo-gite-BLANC.png" alt="Accueil" width="160" height="60" class="d-inline-block align-text-top" title="Accueil">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/chambre/chambre-index.html">Nos chambres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/repas/repas-index.html">Notre restaurant</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/cheval/cheval-index.html">Randonnée équestre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/kayak/kayak-index.html">Sortie en kayak</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/bagne/bagne-index.html">Visite du bagne</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="./modules/garderie/garderie-index.html">La garderie</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-
-				<!-- TODO : burger mobile -->
-				<nav class="navbar mobile">
-
-				</nav>
-			</header>
 		`;
 	}
 }
